8daedbeaa527a48346cb1e034d86dcd4d9cc118a
SURESHKUMAR-T <sureshkumar.he@enchhash.com>
2024-11-15 17:18:23 +0530

    update:completed

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,16 +7,16 @@ const Services = () => {
     <Title title="Services I Offer" sub="Take advantage of the opportunity to explore my skills through various sample projects!"/>
     <div className='service'>
         <div className="service-box">
-          <p>Web Development</p>
-          <p>Utilizing cutting-edge technologies and best practices, I develop responsive and user-friendly websites tailored to meet your specific needs, ensuring a seamless online experience.</p>
+          <p className='service-head'>Web Development</p>
+          <p className='service-body'>Utilizing cutting-edge technologies and best practices, I develop responsive and user-friendly websites tailored to meet your specific needs, ensuring a seamless online experience.</p>
         </div>
         <div className="service-box">
-          <p>UI/UX Design</p>
-          <p>I specialize in creating intuitive and aesthetically pleasing interfaces that enhance user experience, making digital interactions enjoyable and effective.</p>
+          <p className='service-head'>UI/UX Design</p>
+          <p className='service-body'>I specialize in creating intuitive and aesthetically pleasing interfaces that enhance user experience, making digital interactions enjoyable and effective.</p>
         </div>
         <div className="service-box">
-          <p>Graphic Design</p>
-          <p>With a keen eye for detail, I produce visually striking graphics that communicate your brand message clearly and effectively, adding value to your projects.</p>
+          <p className='service-head'>Graphic Design</p>
+          <p className='service-body'>With a keen eye for detail, I produce visually striking graphics that communicate your brand message clearly and effectively, adding value to your projects.</p>
         </div>
     </div>
     </div>
