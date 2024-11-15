0d879e3866251a47723a5d38d2ff8b9f8e5bc48d
SURESHKUMAR-T <sureshkumar.he@enchhash.com>
2024-11-15 17:51:22 +0530

    update:completed

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,15 +8,15 @@ const Services = () => {
     <div className='service'>
         <div className="service-box">
           <p className='service-head' style={{color:"#8C4BFF"}}>Web Development</p>
-          <p className='service-body'>Utilizing cutting-edge technologies and best practices, I develop responsive and user-friendly websites tailored to meet your specific needs, ensuring a seamless online experience.</p>
+          <p className='service-body'  style={{color:"#ffffff"}}>Utilizing cutting-edge technologies and best practices, I develop responsive and user-friendly websites tailored to meet your specific needs, ensuring a seamless online experience.</p>
         </div>
         <div className="service-box">
           <p className='service-head' style={{color:"#8C4BFF"}}>UI/UX Design</p>
-          <p className='service-body'>I specialize in creating intuitive and aesthetically pleasing interfaces that enhance user experience, making digital interactions enjoyable and effective.</p>
+          <p className='service-body' style={{color:"#ffffff"}}>I specialize in creating intuitive and aesthetically pleasing interfaces that enhance user experience, making digital interactions enjoyable and effective.</p>
         </div>
         <div className="service-box">
           <p className='service-head' style={{color:"#8C4BFF"}}>Graphic Design</p>
-          <p className='service-body'>With a keen eye for detail, I produce visually striking graphics that communicate your brand message clearly and effectively, adding value to your projects.</p>
+          <p className='service-body' style={{color:"#ffffff"}}>With a keen eye for detail, I produce visually striking graphics that communicate your brand message clearly and effectively, adding value to your projects.</p>
         </div>
     </div>
     </div>
